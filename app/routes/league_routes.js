81d5c8eac61797d17c167512a5710ef22d3c34fb
austintorres <torresgaustin@gmail.com>
2020-06-30 13:07:20 -0400

    User and league routes CRUD adjust

--- app/routes/league_routes.js
+++ app/routes/league_routes.js
@@ -1,114 +1,79 @@
-// Express docs: http://expressjs.com/en/api.html
 const express = require('express')
-// Passport docs: http://www.passportjs.org/docs/
 const passport = require('passport')
 
-// pull in Mongoose model for leagues
+// mongoose model for league
 const League = require('../models/league')
 
-// this is a collection of methods that help us detect situations when we need
-// to throw a custom error
 const customErrors = require('../../lib/custom_errors')
 
-// we'll use this function to send 404 when non-existant document is requested
 const handle404 = customErrors.handle404
-// we'll use this function to send 401 when a user tries to modify a resource
-// that's owned by someone else
 const requireOwnership = customErrors.requireOwnership
-
-// this is middleware that will remove blank fields from `req.body`, e.g.
-// { example: { title: '', text: 'foo' } } -> { example: { text: 'foo' } }
 const removeBlanks = require('../../lib/remove_blank_fields')
-// passing this as a second argument to `router.<verb>` will make it
-// so that a token MUST be passed for that route to be available
-// it will also set `req.user`
 const requireToken = passport.authenticate('bearer', { session: false })
 
-// instantiate a router (mini app that only handles routes)
+// instantiate a router
 const router = express.Router()
 
 // INDEX
 // GET /leagues
 router.get('/leagues', (req, res, next) => {
   League.find()
+    .populate('owner')
     .then(leagues => {
-      // `leagues` will be an array of Mongoose documents
-      // we want to convert each one to a POJO, so we use `.map` to
-      // apply `.toObject` to each one
       return leagues.map(league => league.toObject())
     })
-    // respond with status 200 and JSON of the leagues
     .then(leagues => res.status(200).json({ leagues: leagues }))
-    // if an error occurs, pass it to the handler
     .catch(next)
 })
 
 // SHOW
-// GET /leagues/5a7db6c74d55bc51bdf39793
+// GET /leagues/4wrewe4546
 router.get('/leagues/:id', requireToken, (req, res, next) => {
-  // req.params.id will be set based on the `:id` in the route
   League.findById(req.params.id)
+    .populate('owner')
     .then(handle404)
-    // if `findById` is succesful, respond with 200 and "league" JSON
     .then(league => res.status(200).json({ league: league.toObject() }))
-    // if an error occurs, pass it to the handler
     .catch(next)
 })
 
 // CREATE
 // POST /leagues
 router.post('/leagues', requireToken, (req, res, next) => {
-  // set owner of new league to be current user
   req.body.league.owner = req.user.id
 
   League.create(req.body.league)
-    // respond to succesful `create` with status 201 and JSON of new "league"
     .then(league => {
       res.status(201).json({ league: league.toObject() })
     })
-    // if an error occurs, pass it off to our error handler
-    // the error handler needs the error message and the `res` object so that it
-    // can send an error message back to the client
     .catch(next)
 })
 
 // UPDATE
-// PATCH /leagues/5a7db6c74d55bc51bdf39793
+// PATCH /leagues/764536564
 router.patch('/leagues/:id', requireToken, removeBlanks, (req, res, next) => {
-  // if the client attempts to change the `owner` property by including a new
-  // owner, prevent that by deleting that key/value pair
   delete req.body.league.owner
 
   League.findById(req.params.id)
+    .populate('owner')
     .then(handle404)
     .then(league => {
-      // pass the `req` object and the Mongoose record to `requireOwnership`
-      // it will throw an error if the current user isn't the owner
       requireOwnership(req, league)
-
-      // pass the result of Mongoose's `.update` to the next `.then`
       return league.updateOne(req.body.league)
     })
-    // if that succeeded, return 204 and no JSON
-    .then(() => res.sendStatus(204))
-    // if an error occurs, pass it to the handler
+    .then(() => res.sendStatus(202))
     .catch(next)
 })
 
 // DESTROY
-// DELETE /leagues/5a7db6c74d55bc51bdf39793
+// DELETE /leagues/532446
 router.delete('/leagues/:id', requireToken, (req, res, next) => {
   League.findById(req.params.id)
     .then(handle404)
     .then(league => {
-      // throw an error if current user doesn't own `league`
       requireOwnership(req, league)
-      // delete the league ONLY IF the above didn't throw
       league.deleteOne()
     })
-    // send back 204 and no content if the deletion succeeded
     .then(() => res.sendStatus(204))
-    // if an error occurs, pass it to the handler
     .catch(next)
 })
 
--- app/routes/user_routes.js
+++ app/routes/user_routes.js
@@ -1,37 +1,22 @@
 const express = require('express')
-// jsonwebtoken docs: https://github.com/auth0/node-jsonwebtoken
 const crypto = require('crypto')
-// Passport docs: http://www.passportjs.org/docs/
 const passport = require('passport')
-// bcrypt docs: https://github.com/kelektiv/node.bcrypt.js
 const bcrypt = require('bcrypt')
-
-// see above for explanation of "salting", 10 rounds is recommended
 const bcryptSaltRounds = 10
 
-// pull in error types and the logic to handle them and set status codes
 const errors = require('../../lib/custom_errors')
-
 const BadParamsError = errors.BadParamsError
 const BadCredentialsError = errors.BadCredentialsError
-
 const User = require('../models/user')
-
-// passing this as a second argument to `router.<verb>` will make it
-// so that a token MUST be passed for that route to be available
-// it will also set `res.user`
 const requireToken = passport.authenticate('bearer', { session: false })
 
-// instantiate a router (mini app that only handles routes)
+// instantiate a router
 const router = express.Router()
 
 // SIGN UP
 // POST /sign-up
 router.post('/sign-up', (req, res, next) => {
-  // start a promise chain, so that any errors will pass to `handle`
   Promise.resolve(req.body.credentials)
-    // reject any requests where `credentials.password` is not present, or where
-    // the password is an empty string
     .then(credentials => {
       if (!credentials ||
           !credentials.password ||
